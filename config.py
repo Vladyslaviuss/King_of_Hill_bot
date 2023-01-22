2988e136a99f81de714dd91a8cf38c2fe5133c78
vladyslavius <marakhovskyiv@gmail.com>
2023-01-23 00:19:11 +0200

    :globe_with_meridians: Adjust env file and apply migrations

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 from sqlalchemy.engine import URL
 
-postgres_url: URL = URL.create(
-    'postgresql+asyncpg',
-    username='postgres',
-    password='qwerty',
-    host='localhost',
-    database='hilldb',
-    port=5432
-)
+# postgres_url: URL = URL.create(
+#     'postgresql+asyncpg',
+#     username='postgres',
+#     password='qwerty',
+#     host='localhost',
+#     database='hilldb',
+#     port=5432
+# )
+
+postgres_url = "postgresql+asyncpg://postgres:qwerty@localhost/hilldb"
