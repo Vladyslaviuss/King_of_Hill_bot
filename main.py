1f9b96d688f3f18d552285e2ba62013277e45d81
vladyslavius <marakhovskyiv@gmail.com>
2023-01-25 10:22:46 +0200

    :construction: Minor changes in functions

--- main.py
+++ main.py
@@ -20,11 +20,6 @@ dp = Dispatcher(bot)
 
 db.init()
 
-async def update(id: str, existed_db_string: StringSchema):
-    same_db_string = await Statistic.update(id, **existed_db_string.dict())
-    return same_db_string
-
-
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -32,9 +27,11 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    result = await get_string(id=message.chat.id)
-    await message.reply(f"Hi!\nI'm Bot!\n First data in db written! {result}")
-
+    member = await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+    if member.status == ChatMemberStatus.OWNER:
+        await message.reply("Hi!\nI'm Bot!\n This is test message.")
+    else:
+        await message.reply("You have no permission")
 
 @dp.message_handler(commands=['results'])
 async def show_results(message: types.Message):
@@ -48,7 +45,7 @@ async def show_results(message: types.Message):
         await message.reply(f"{message_text}")
     else:
         await bot.send_message(
-            chat_id=message.chat.id, text=f'Currently no any results for chat {message.chat.full_name}.'
+            chat_id=message.chat.id, text=f'Currently no any results for chat "{message.chat.full_name}".'
         )
 
 @dp.message_handler(content_types=ContentType.TEXT)
@@ -60,7 +57,7 @@ async def handle_text(message: Message):
             if not message.reply_to_message.from_user.is_bot:
                 if await check_if_exists(id=message.chat.id) is None:
                     await create_new_string(id=message.chat.id, new_db_string=StringSchema(analysis=0, signals=0, screenshot=0, help=0))
-                    message_text = f'Записи не найдены. Параметры для Вашего чата: {message.chat.full_name} сгенерированы. Повторите последнее действие для его приме'
+                    message_text = f'Записи не найдены. Параметры для Вашего чата: "{message.chat.full_name}" сгенерированы. Повторите последнее действие для его применения.'
                     await bot.send_message(chat_id=message.chat.id, text=message_text)
                 else:
                     result = await update_the_value_of_object(id=message.chat.id, text=text)
